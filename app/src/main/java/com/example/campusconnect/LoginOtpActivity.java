c06a0d8d8493b13a5c2be26cf896605a3a9cbc9d
fsRakib <fsrakiba50@gmail.com>
2023-10-18 14:10:44 +0600

    OTP Function developed_2

--- app/src/main/java/com/example/campusconnect/LoginOtpActivity.java
+++ app/src/main/java/com/example/campusconnect/LoginOtpActivity.java
@@ -3,6 +3,7 @@ package com.example.campusconnect;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.campusconnect.utils.AndroidUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
@@ -46,6 +50,13 @@ public class LoginOtpActivity extends AppCompatActivity {
 
         phoneNumber=getIntent().getExtras().getString("phone");
        sendOtp(phoneNumber, false);
+
+       nextBtn.setOnClickListener(v->{
+           String enteredOtp= otpInput.getText().toString();
+           PhoneAuthCredential credential= PhoneAuthProvider.getCredential(verificationCode, enteredOtp);
+           signIn(credential);
+           setInProgress(true);
+       });
 
     }
     void  sendOtp(String phoneNumber, boolean isResend)
@@ -103,5 +114,21 @@ public class LoginOtpActivity extends AppCompatActivity {
     void signIn(PhoneAuthCredential phoneAuthCredential)
     {
         //login and goto next activity
+        setInProgress(true);
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Intent intent=new Intent(LoginOtpActivity.this, LoginUsernameActivity.class);
+                    intent.putExtra("phone", phoneNumber);
+                    startActivity(intent);
+                }
+                else
+                {
+                    AndroidUtil.showToast(getApplicationContext(),"OTP verification failed");
+                }
+            }
+        });
     }
 }
